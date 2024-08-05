906e91f6d13a28273f6830e4010c9ab7f8c37776
wuxijiazu <116379571+wuxijiazu@users.noreply.github.com>
2024-08-05 17:34:03 +0800

    Add files via upload

--- Crane_Gripper_Two/src/main.cpp
+++ Crane_Gripper_Two/src/main.cpp
@@ -1,24 +1,25 @@
 #include <Arduino.h>
 
-#include "steeping42.hpp"
+#include "EMMC42V53.hpp"
 #include "gripper.hpp"
 #include "esp_now_community.hpp"
 #include "status.hpp"
 #include "servo.hpp"
 
-Servo servo_gripper;
+ESP_Now_e eps_now_e;
 Esp_Now_Community esp_now_community;
-Steeping42 steeping42_motor_1;
-Steeping42 steeping42_motor_2;
+EMMC423V53 motor_1;
+EMMC423V53 motor_2;
 Gripper gripper_two;
 Status state_machine;
+Servo servo_gripper;
 
 void setup() {
   Serial.begin(115200);
   // delay(2000);
   // Serial.begin(115200,SERIAL_8N1,34,35);
   esp_now_community.Esp_Now_Init();
-  gripper_two.Gripper_Motor_Init(&steeping42_motor_1, &steeping42_motor_2, &Serial1, 25, 26);
+  gripper_two.Gripper_Motor_Init(&motor_1, &motor_2, &Serial1, 25, 26);
   gripper_two.Gripper_Servo_Init(&servo_gripper, 0, 33);
   state_machine.State_Init();
   // servo_gripper.Servo_Init(0,33);
